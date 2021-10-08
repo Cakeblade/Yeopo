7ac216e3ba4bfcf11990504f9d9c4e09a653038b
뜨라네 <jch1070@gmail.com>
2021-10-08 14:37:09 +0000

    10/08 23:36 중대 시스템 완성 / 탐색 구현 중

--- class.js
+++ class.js
@@ -16,7 +16,7 @@ class Person // Node of linked list
 	//setter
 	setName(_name) {this.val[0] = _name;}
 	setPos(_Pos) {this.val[1] = _Pos;}
-	setTyp(_Typ) {this.val[2] = _Typ;}
+	setTyp(_Typ) {this.val[2] = _Typ;} // 용사 0, 간부 1
 	setNext(_level) {this.next = _level;}
 	
 }
@@ -69,19 +69,20 @@ class Company
 		curNode.next = _mans;
 		this.nDorm[_iDorm - 1] = this.nDorm[_iDorm - 1] + 1;
 	}
-	/*
+	
 	insertAt(_mans, _iDorm, _index) //인원, 생활관, 넣을 자리
 	{
 		let preNode;
 		let curNode = this.dorm[_iDorm-1];
 		let count = 0;
 		
-		if(_index > 0 && _index > this.nDorm[_iDorm-1]) //index가 인원수보다 많으면
+		if(_index - 1 > 0 && _index - 1 > this.nDorm[_iDorm-1]) //index가 인원수보다 많으면
 		{
+			document.writeln("Error!");
 			return;
 		}
 		
-		if(_index == 0) // 맨 앞에 넣을거면
+		if(_index == 1) // 맨 앞에 넣을거면
 		{
 			_mans.setNext(this.dorm[_iDorm-1]);
 			this.nDorm[_iDorm-1]++;
@@ -89,16 +90,16 @@ class Company
 			return;
 		}
 		
-		while(count < _index) // index Node까지 이동
+		while(count < _index - 1) // index Node까지 이동
 		{
 			preNode = curNode;
 			count++;
-			curNode.setNext(curNode.getNext());
+			curNode = curNode.getNext();
 		}
 		_mans.setNext(curNode);
 		preNode.setNext(_mans);
 		this.nDorm[_iDorm-1]++;
-	}*/
+	}
 	
 	getAt(_iDorm, _index)
 	{
@@ -117,7 +118,7 @@ class Company
 		return null;
 	}
 	
-	removeAt() // 나중에 필요할 때 만들기
+	removeAt() // 나중에 필요할 때 만들기 - file i/o 까지 다 구현하고 html단에서의 중대원 추가/삭제 구현시 만질 듯
 	{
 		
 	}
@@ -125,39 +126,78 @@ class Company
 	printList(_iDorm) // 사실상 test용 함수 -- 나중에 이름 배열을 return하는 함수가 될듯?
 	{
 		let curNode = this.dorm[_iDorm-1];
-		while(curNode?.getNext() != null)
+		while(curNode?.getNext() != null) // ? 안붙이면 오류난다. ㅁㄴㅇㄹ
 		{
 			document.writeln(curNode.getName());
 			curNode = curNode.getNext();
 		}
+		document.writeln(curNode.getName()); // 이거 안적으면 맨 마지막 노드가 안나온다. 해결 방법이 필요한듯.
 		document.writeln();
+	}
+	
+	findPersonByName(_name) // 이름으로 Person 객체를 찾는 함수 - return -> Person Node
+	{
+		let cnt = 0;
+		while(this.dorm[cnt])
+		{
+			let curNode = this.dorm[cnt];
+			while(curNode?.getNext() != null) // ? 안붙이면 오류난다. ㅁㄴㅇㄹ
+			{
+				if(curNode.getName() == _name)
+				{
+					document.writeln(curNode.getName());
+				}
+				curNode = curNode.getNext();
+			}
+			if(curNode.getName() == _name)
+			{
+				document.writeln(curNode.getName());
+			}
+			cnt++;
+		}
+		return;
+	}
+	
+	findPersonByPos(_pos) // 위치로 Person 객체를 찾는 함수 - return -> Person Node
+	{
+		let cnt = 0;
+		while(this.dorm[cnt])
+		{
+			let curNode = this.dorm[cnt];
+			while(curNode?.getNext() != null) // ? 안붙이면 오류난다. ㅁㄴㅇㄹ
+			{
+				if(curNode.getPos() == _pos)
+				{
+					document.writeln(curNode.getPos());
+				}
+				curNode = curNode.getNext();
+			}
+			if(curNode.getPos() == _pos)
+			{
+				document.writeln(curNode.getPos());
+			}
+			cnt++;
+		}
+		return;
 	}
 }
 
-
-const p1 = new Person("jch","dorm","0");
-const p2 = new Person("jhc","dorm","0");
-const p3 = new Person("abc","dorm","0");
-const p4 = new Person("cba","dorm","0");
-
-const p5 = new Person("qqq","dorm","0");
-const p6 = new Person("www","dorm","0");
-const p7 = new Person("eee","dorm","0");
-const p8 = new Person("rrr","dorm","0");
-
-const sig = new Company("SIG", p1);
-
-sig.makeNewDorm(p5);
-
-sig.insertLast(p2, 1);
-sig.insertLast(p3, 1);
-sig.insertLast(p4, 1);
-//sig.insertAt(p3, 1, 3)
-
-sig.insertLast(p6, 2);
-sig.insertLast(p7, 2);
-sig.insertLast(p8, 2);
-//sig.insertAt(p7, 2, 3)
-
-sig.printList(1);
-sig.printList(2);
+class Place
+{
+	constructor(_name, _NoP, _type)
+	{
+		this.name = _name;
+		this.NoP = _NoP;
+		this.type = _type; // 영내는 1, 영외는 2
+	}
+	
+	//getter
+	getName() {return this.name;}
+	getNoP() {return this.NoP;}
+	getType() {return this.type;}
+	
+	//setter
+	setName(_name) {this.name = _name;}
+	setNoP(_NoP) {this.NoP = _NoP;}
+	setType(_type) {this.type = _type}
+}
---
+++ fio.js
@@ -0,0 +1,11 @@
+document.write("<script src='./class.js'></script>")
+
+function dirInCount()
+{
+	
+}
+
+function loadCompany(brigade)
+{
+	
+}
