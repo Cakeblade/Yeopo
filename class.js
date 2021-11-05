8e4006cd64b1a3bf7ebc258aa361bad4a2370bcb
뜨라네 <jch1070@gmail.com>
2021-11-05 14:35:36 +0000

    11/05 23:35

--- class.js
+++ class.js
@@ -47,12 +47,9 @@ class Company
 	setNumPer(_NumPer) {this.NumPer = _NumPer;}
 	
 	// function related to NumPer
-	calcNumPer()
+	calcNumPer() // 다시 짤 예정
 	{
-		var cnt = 0;
-		for(let i = 0; i < this.numDorm; i++) cnt = cnt + this.nDorm[i];
-		this.numPer = cnt;
-		return cnt;
+		
 	}
 	
 	// function related to Dorm list
@@ -63,24 +60,24 @@ class Company
 		this.setNumDorm(this.getNumDorm() + 1);
 	}
 	
-	insertLast(_mans, _iDorm) //인원, 생활관 / 매개변수는 1, 2 이렇게 받을 예정이기 때문에 밑에서 -1을 해준다. (아니면 간부 목록을 0으로 넣고 1부터 index를 시작해도 될 듯?)
+	insertLast(_mans, _iDorm) //인원, 생활관 간부 목록을 0으로 넣고 1부터 index를 시작한다.
 	{
-		let curNode = this.getDorm(_iDorm - 1);
+		let curNode = this.getDorm(_iDorm);
 		while(curNode.getNext())
 		{
 			curNode = curNode.getNext();
 		}
 		curNode.next = _mans;
-		this.nDorm[_iDorm - 1] = this.nDorm[_iDorm - 1] + 1;
+		this.nDorm[_iDorm] = this.nDorm[_iDorm] + 1;
 	}
 	
 	insertAt(_mans, _iDorm, _index) //인원, 생활관, 넣을 자리
 	{
 		let preNode;
-		let curNode = this.getDorm(_iDorm - 1);
+		let curNode = this.getDorm(_iDorm);
 		let count = 0;
 		
-		if(_index - 1 > 0 && _index - 1 > this.nDorm[_iDorm-1]) //index가 인원수보다 많으면
+		if(_index - 1 > 0 && _index - 1 > this.nDorm[_iDorm]) //index가 인원수보다 많으면
 		{
 			document.writeln("Error!");
 			return;
@@ -88,9 +85,9 @@ class Company
 		
 		if(_index == 1) // 맨 앞에 넣을거면
 		{
-			_mans.setNext(this.getDorm(_iDorm - 1));
-			this.nDorm[_iDorm-1]++;
-			this.dorm[_iDorm - 1] = _mans;
+			_mans.setNext(this.getDorm(_iDorm));
+			this.nDorm[_iDorm]++;
+			this.dorm[_iDorm] = _mans;
 			return;
 		}
 		
@@ -102,12 +99,12 @@ class Company
 		}
 		_mans.setNext(curNode);
 		preNode.setNext(_mans);
-		this.nDorm[_iDorm-1]++;
+		this.nDorm[_iDorm]++;
 	}
 	
 	getAt(_iDorm, _index)
 	{
-		let curNode = this.getDorm(_iDorm - 1);
+		let curNode = this.getDorm(_iDorm);
 		let count = 0;
 		
 		while(curNode)
@@ -129,7 +126,7 @@ class Company
 	
 	printList(_iDorm) // 사실상 test용 함수 -- 나중에 이름 배열을 return하는 함수가 될듯?
 	{
-		let curNode = this.getDorm(_iDorm - 1);
+		let curNode = this.getDorm(_iDorm);
 		while(curNode?.getNext() != null) // ? 안붙이면 오류난다. ㅁㄴㅇㄹ
 		{
 			document.writeln(curNode.getName());
@@ -137,6 +134,7 @@ class Company
 		}
 		document.writeln(curNode.getName()); // 이거 안적으면 맨 마지막 노드가 안나온다. 해결 방법이 필요한듯.
 		document.writeln();
+		//alert(curNode.getName())
 	}
 	
 	returnList(cnt) // 생활관 인원 리스트를 return?
@@ -269,3 +267,34 @@ class Place
 	setNoP(_NoP) {this.NoP = _NoP;}
 	setType(_type) {this.type = _type}
 }
+
+// 테스트 중대
+var p0 = new Person("황상윤", 0, 1)
+var p1 = new Person("강경주", 0, 1)
+var p2 = new Person("추민선", 1, 0)
+var p3 = new Person("허예지", 1, 0)
+var p4 = new Person("전영철", 1, 0)
+var p5 = new Person("조만수", 1, 0)
+var p6 = new Person("유승희", 2, 0)
+var p7 = new Person("전은하", 2, 0)
+var p8 = new Person("장혜연", 2, 0)
+var p9 = new Person("오경환", 2, 0)
+var p10 = new Person("윤홍식", 3, 0)
+var p11 = new Person("남해주", 3, 0)
+var p12 = new Person("임기연", 3, 0)
+var p13 = new Person("유혜연", 3, 0)
+
+var sig = new Company("sig", p0); // 0
+sig.makeNewDorm(p2); // 1
+sig.makeNewDorm(p6); // 2
+sig.makeNewDorm(p10); // 3
+
+sig.insertLast(p3, 1);
+sig.insertLast(p4, 1);
+sig.insertLast(p5, 1);
+sig.insertLast(p7, 2);
+sig.insertLast(p8, 2);
+sig.insertLast(p9, 2);
+sig.insertLast(p11, 3);
+sig.insertLast(p12, 3);
+sig.insertLast(p13, 3);
